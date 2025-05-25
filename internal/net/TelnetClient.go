d0696eb43afa101f59e4959a5ac3cb92acf770ec
wscalf <wscalf@gmail.com>
2025-05-25 16:15:28 -0400

    Ansi color (#2)
    * Initial PoC
    
    * Add italics and cache static formatting codes
    
    * Simplification, benchmarks, and bugfixes

--- internal/net/TelnetClient.go
+++ internal/net/TelnetClient.go
@@ -2,21 +2,24 @@ package net
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"strings"
 
 	"github.com/reiver/go-telnet"
 	"github.com/wscalf/tbdmud/internal/game"
+	"github.com/wscalf/tbdmud/internal/text"
 )
 
 const (
-	CR byte = 13
-	LF byte = 10
+	CR  byte = 13
+	LF  byte = 10
+	ESC byte = 27
 )
 
 type TelnetClient struct {
 	reader     telnet.Reader
-	writer     telnet.Writer
+	writer     *text.MarkupFilter
 	input      chan string
 	disconnect bool
 	lastError  error
@@ -25,7 +28,7 @@ type TelnetClient struct {
 func newTelnetClient(reader telnet.Reader, writer telnet.Writer) *TelnetClient {
 	return &TelnetClient{
 		reader: reader,
-		writer: writer,
+		writer: text.NewMarkupFilter(writer, handleANSIFormattingReplacement),
 	}
 }
 
@@ -81,4 +84,126 @@ func (t *TelnetClient) Run() {
 
 func (t *TelnetClient) Disconnect() {
 	t.disconnect = true
+}
+
+const sgrPattern string = "\033[%sm"
+
+var ansiBold string = fmt.Sprintf(sgrPattern, "1")
+var ansiFaint string = fmt.Sprintf(sgrPattern, "2")
+var ansiItalic string = fmt.Sprintf(sgrPattern, "3")
+var ansiUnderline string = fmt.Sprintf(sgrPattern, "4")
+var ansiStrikeout string = fmt.Sprintf(sgrPattern, "9")
+var ansiClearBoldFaint string = fmt.Sprintf(sgrPattern, "22")
+var ansiClearItalic string = fmt.Sprintf(sgrPattern, "23")
+var ansiClearUnderline string = fmt.Sprintf(sgrPattern, "24")
+var ansiClearStrikeout string = fmt.Sprintf(sgrPattern, "29")
+var ansiForecolorBlack string = fmt.Sprintf(sgrPattern, "30")
+var ansiForecolorRed string = fmt.Sprintf(sgrPattern, "31")
+var ansiForecolorGreen string = fmt.Sprintf(sgrPattern, "32")
+var ansiForecolorYellow string = fmt.Sprintf(sgrPattern, "33")
+var ansiForecolorBlue string = fmt.Sprintf(sgrPattern, "34")
+var ansiForecolorMagenta string = fmt.Sprintf(sgrPattern, "35")
+var ansiForecolorCyan string = fmt.Sprintf(sgrPattern, "36")
+var ansiForecolorWhite string = fmt.Sprintf(sgrPattern, "37")
+var ansiClearforecolor string = fmt.Sprintf(sgrPattern, "39")
+var ansiBackcolorBlack string = fmt.Sprintf(sgrPattern, "40")
+var ansiBackcolorRed string = fmt.Sprintf(sgrPattern, "41")
+var ansiBackcolorGreen string = fmt.Sprintf(sgrPattern, "42")
+var ansiBackcolorYellow string = fmt.Sprintf(sgrPattern, "43")
+var ansiBackcolorBlue string = fmt.Sprintf(sgrPattern, "44")
+var ansiBackcolorMagenta string = fmt.Sprintf(sgrPattern, "45")
+var ansiBackcolorCyan string = fmt.Sprintf(sgrPattern, "46")
+var ansiBackcolorWhite string = fmt.Sprintf(sgrPattern, "47")
+var ansiClearBackcolor string = fmt.Sprintf(sgrPattern, "49")
+
+func handleANSIFormattingReplacement(directive text.FormattingDirective) string {
+	switch directive.FormattingKind {
+	case text.FormattingKindForecolor:
+		if directive.End {
+			return ansiClearforecolor
+		}
+
+		switch directive.Param {
+		case "black":
+			return ansiForecolorBlack
+		case "red":
+			return ansiForecolorRed
+		case "green":
+			return ansiForecolorGreen
+		case "yellow":
+			return ansiForecolorYellow
+		case "blue":
+			return ansiForecolorBlue
+		case "magenta":
+			return ansiForecolorMagenta
+		case "cyan":
+			return ansiForecolorCyan
+		case "white":
+			return ansiForecolorWhite
+		default:
+			return ansiForecolorWhite
+		}
+	case text.FormattingKindBackcolor:
+		if directive.End {
+			return ansiClearBackcolor
+		}
+
+		switch directive.Param {
+		case "black":
+			return ansiBackcolorBlack
+		case "red":
+			return ansiBackcolorRed
+		case "green":
+			return ansiBackcolorGreen
+		case "yellow":
+			return ansiBackcolorYellow
+		case "blue":
+			return ansiBackcolorBlue
+		case "magenta":
+			return ansiBackcolorMagenta
+		case "cyan":
+			return ansiBackcolorCyan
+		case "white":
+			return ansiBackcolorWhite
+		default:
+			return ansiBackcolorBlack
+		}
+
+	case text.FormattingKindBold:
+		if directive.End {
+			return ansiClearBoldFaint
+		} else {
+			return ansiBold
+		}
+
+	case text.FormattingKindFaint:
+		if directive.End {
+			return ansiClearBoldFaint
+		} else {
+			return ansiFaint
+		}
+
+	case text.FormattingKindItalic:
+		if directive.End {
+			return ansiClearItalic
+		} else {
+			return ansiItalic
+		}
+
+	case text.FormattingKindUnderline:
+		if directive.End {
+			return ansiClearUnderline
+		} else {
+			return ansiUnderline
+		}
+
+	case text.FormattingKindStrikeout:
+		if directive.End {
+			return ansiClearStrikeout
+		} else {
+			return ansiStrikeout
+		}
+	}
+
+	return ""
 }
---
+++ internal/text/MarkupFilter_test.go
@@ -0,0 +1,88 @@
+package text
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarkupFilterPassesThroughUnmarkedText(t *testing.T) {
+	sb := &strings.Builder{}
+	filter := NewMarkupFilter(sb, nil)
+
+	input := "Foo"
+	filter.Write([]byte(input))
+	output := sb.String()
+
+	assert.Equal(t, input, output)
+}
+func TestMarkupFilterSkipsUnhandledDirectives(t *testing.T) {
+	sb := &strings.Builder{}
+	filter := NewMarkupFilter(sb, nil)
+
+	input := "[fc=red]F[i]o[/i]o[/fc]"
+	filter.Write([]byte(input))
+	output := sb.String()
+
+	assert.Equal(t, "Foo", output)
+}
+
+func TestMarkupFilterHandlesReplacements(t *testing.T) {
+	sb := &strings.Builder{}
+	filter := NewMarkupFilter(sb, handleReplacement)
+
+	input := "[fc=magenta]F[i]o[/i]o[/fc]"
+	filter.Write([]byte(input))
+	output := sb.String()
+
+	assert.Equal(t, "(magenta)F(i)o(!i)o(!color)", output)
+}
+
+func BenchmarkMarkupFilterWrite(b *testing.B) {
+	sb := &strings.Builder{}
+	filter := NewMarkupFilter(sb, handleReplacement)
+
+	for i := 0; i < b.N; i++ {
+		filter.Write([]byte("[fc=red]F[i]o[/i]o[/fc]"))
+		sb.Reset()
+	}
+}
+
+func TestMarkupFilterHandlesDirectivesSplitOverWriteCalls(t *testing.T) {
+	sb := &strings.Builder{}
+	filter := NewMarkupFilter(sb, handleReplacement)
+
+	// Original input: "[fc=red]F[i]o[/i]o[/fc]"
+	filter.Write([]byte("["))
+	filter.Write([]byte("fc=red"))
+	filter.Write([]byte("]F[i"))
+	filter.Write([]byte("]o[/i]o["))
+	filter.Write([]byte("/"))
+	filter.Write([]byte("f"))
+	filter.Write([]byte("c"))
+	filter.Write([]byte("]"))
+	output := sb.String()
+
+	assert.Equal(t, "(red)F(i)o(!i)o(!color)", output)
+}
+
+func handleReplacement(directive FormattingDirective) string {
+	switch directive.FormattingKind {
+	case FormattingKindForecolor:
+		if directive.End {
+			return "(!color)"
+		} else {
+			return fmt.Sprintf("(%s)", directive.Param)
+		}
+	case FormattingKindItalic:
+		if directive.End {
+			return "(!i)"
+		} else {
+			return "(i)"
+		}
+	default:
+		return ""
+	}
+}

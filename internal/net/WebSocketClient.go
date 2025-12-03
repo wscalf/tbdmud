c004b3a6b21cdb2bb999bc6cc3f6025495fcc1b0
William Scalf <wscalf@gmail.com>
2025-12-03 17:18:47 -0500

    Add text formatting support for web clients

--- internal/net/WebSocketClient.go
+++ internal/net/WebSocketClient.go
@@ -1,10 +1,12 @@
 package net
 
 import (
+	"fmt"
 	"log/slog"
 
 	"github.com/gorilla/websocket"
 	"github.com/wscalf/tbdmud/internal/game"
+	"github.com/wscalf/tbdmud/internal/text"
 )
 
 type WebSocketClient struct {
@@ -26,10 +28,12 @@ func (c *WebSocketClient) Send(msg game.OutputJob) error {
 	if err != nil {
 		return err
 	}
-
 	defer writer.Close()
 
-	err = msg.Run(writer)
+	formatter := text.NewMarkupFilter(writer, handleHTMLFormattingReplacement)
+	escaper := NewHTMLEscapeFilter(formatter)
+
+	err = msg.Run(escaper)
 	return err
 }
 
@@ -64,4 +68,46 @@ func (c *WebSocketClient) Run() {
 
 		c.input <- body
 	}
+}
+
+func handleHTMLFormattingReplacement(directive text.FormattingDirective) string {
+	switch directive.FormattingKind {
+	case text.FormattingKindForecolor:
+		if directive.End {
+			return "</span>"
+		} else {
+			return fmt.Sprintf("<span style=\"color: %s\">", directive.Param) //This may need to be validated to be a color
+		}
+	case text.FormattingKindBackcolor:
+		if directive.End {
+			return "</span>"
+		} else {
+			return fmt.Sprintf("<span style=\"background-color: %s\">", directive.Param) //This may need to be validated to be a color
+		}
+	case text.FormattingKindBold:
+		if directive.End {
+			return "</b>"
+		} else {
+			return "<b>"
+		}
+	case text.FormattingKindItalic:
+		if directive.End {
+			return "</i>"
+		} else {
+			return "<i>"
+		}
+	case text.FormattingKindUnderline:
+		if directive.End {
+			return "</u>"
+		} else {
+			return "<u>"
+		}
+	case text.FormattingKindStrikeout:
+		if directive.End {
+			return "</s>"
+		} else {
+			return "<s>"
+		}
+	}
+	return ""
 }
